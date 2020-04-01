06678e82185dec943b4f94fef9f9629058cf4c5c
mcqueg <garrettmccue@gmail.com>
2020-04-01 14:22:30 -0400

    one more problem finished from python-village

---
+++ Python-Village/working-with-files/working-w-files.py
@@ -0,0 +1,14 @@
+##!/usr/bin/env python
+
+f_out = open('output.txt', 'w+')
+f_in = open('rosalind_ini5.txt', 'r')
+
+i = 1
+for line in f_in.readlines():
+    if i % 2 == 0:
+        f_out.write(str(line))
+    i += 1
+
+
+f_out.close()
+print("k")
